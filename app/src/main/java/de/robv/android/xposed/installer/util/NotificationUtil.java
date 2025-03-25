a0d2bff8fe40201340a5c09be48cb988cb8d2276
fish47 <fish47@126.com>
2025-03-25 19:35:33 +0800

    Remove shell dependecy

--- app/src/main/java/de/robv/android/xposed/installer/util/NotificationUtil.java
+++ app/src/main/java/de/robv/android/xposed/installer/util/NotificationUtil.java
@@ -19,10 +19,6 @@ public final class NotificationUtil {
     public static final int NOTIFICATION_MODULES_UPDATED = 1;
     private static final int PENDING_INTENT_OPEN_MODULES = 0;
     private static final int PENDING_INTENT_OPEN_INSTALL = 1;
-    private static final int PENDING_INTENT_SOFT_REBOOT = 2;
-    private static final int PENDING_INTENT_REBOOT = 3;
-    private static final int PENDING_INTENT_ACTIVATE_MODULE_AND_REBOOT = 4;
-    private static final int PENDING_INTENT_ACTIVATE_MODULE = 5;
     private static Context sContext = null;
     private static NotificationManager sNotificationManager;
 
@@ -33,14 +29,6 @@ public final class NotificationUtil {
 
         sContext = XposedApp.getInstance();
         sNotificationManager = (NotificationManager) sContext.getSystemService(Context.NOTIFICATION_SERVICE);
-    }
-
-    public static void cancel(int id) {
-        sNotificationManager.cancel(id);
-    }
-
-    public static void cancelAll() {
-        sNotificationManager.cancelAll();
     }
 
     public static void showNotActivatedNotification(String packageName,
@@ -61,32 +49,12 @@ public final class NotificationUtil {
         if (Build.VERSION.SDK_INT >= 21)
             builder.setPriority(2);
 
-        Intent iActivateAndReboot = new Intent(sContext, RebootReceiver.class);
-        iActivateAndReboot.putExtra(RebootReceiver.EXTRA_ACTIVATE_MODULE, packageName);
-        PendingIntent pActivateAndReboot = PendingIntent.getBroadcast(sContext,
-                PENDING_INTENT_ACTIVATE_MODULE_AND_REBOOT, iActivateAndReboot,
-                PendingIntent.FLAG_UPDATE_CURRENT);
-
-        Intent iActivate = new Intent(sContext, RebootReceiver.class);
-        iActivate.putExtra(RebootReceiver.EXTRA_ACTIVATE_MODULE, packageName);
-        iActivate.putExtra(RebootReceiver.EXTRA_ACTIVATE_MODULE_AND_RETURN, true);
-        PendingIntent pActivate = PendingIntent.getBroadcast(sContext,
-                PENDING_INTENT_ACTIVATE_MODULE, iActivate,
-                PendingIntent.FLAG_UPDATE_CURRENT);
-
         NotificationCompat.BigTextStyle notiStyle = new NotificationCompat.BigTextStyle();
         notiStyle.setBigContentTitle(title);
         notiStyle.bigText(sContext.getString(
                 R.string.module_is_not_activated_yet_detailed, appName));
         builder.setStyle(notiStyle);
 
-        // Only show the quick activation button if any module has been
-        // enabled before,
-        // to ensure that the user know the way to disable the module later.
-        if (!ModuleUtil.getInstance().getEnabledModules().isEmpty()) {
-            builder.addAction(R.drawable.ic_menu_refresh, sContext.getString(R.string.activate_and_reboot), pActivateAndReboot);
-            builder.addAction(R.drawable.ic_save, sContext.getString(R.string.activate_only), pActivate);
-        }
 
         sNotificationManager.notify(packageName,
                 NOTIFICATION_MODULE_NOT_ACTIVATED_YET, builder.build());
@@ -116,62 +84,6 @@ public final class NotificationUtil {
         if (Build.VERSION.SDK_INT >= 21)
             builder.setPriority(2);
 
-        Intent iSoftReboot = new Intent(sContext, RebootReceiver.class);
-        iSoftReboot.putExtra(RebootReceiver.EXTRA_SOFT_REBOOT, true);
-        PendingIntent pSoftReboot = PendingIntent.getBroadcast(sContext,
-                PENDING_INTENT_SOFT_REBOOT, iSoftReboot,
-                PendingIntent.FLAG_UPDATE_CURRENT);
-
-        Intent iReboot = new Intent(sContext, RebootReceiver.class);
-        PendingIntent pReboot = PendingIntent.getBroadcast(sContext,
-                PENDING_INTENT_REBOOT, iReboot,
-                PendingIntent.FLAG_UPDATE_CURRENT);
-
-        builder.addAction(0, sContext.getString(R.string.reboot), pReboot);
-        builder.addAction(0, sContext.getString(R.string.soft_reboot),
-                pSoftReboot);
-
         sNotificationManager.notify(null, NOTIFICATION_MODULES_UPDATED, builder.build());
-    }
-
-    public static class RebootReceiver extends BroadcastReceiver {
-        public static String EXTRA_SOFT_REBOOT = "soft";
-        public static String EXTRA_ACTIVATE_MODULE = "activate_module";
-        public static String EXTRA_ACTIVATE_MODULE_AND_RETURN = "activate_module_and_return";
-
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            /*
-            * Close the notification bar in order to see the toast that module
-            * was enabled successfully. Furthermore, if SU permissions haven't
-            * been granted yet, the SU dialog will be prompted behind the
-            * expanded notification panel and is therefore not visible to the
-            * user.
-            */
-            sContext.sendBroadcast(new Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS));
-            cancelAll();
-
-            if (intent.hasExtra(EXTRA_ACTIVATE_MODULE)) {
-                String packageName = intent.getStringExtra(EXTRA_ACTIVATE_MODULE);
-                ModuleUtil moduleUtil = ModuleUtil.getInstance();
-                moduleUtil.setModuleEnabled(packageName, true);
-                moduleUtil.updateModulesList(false);
-                Toast.makeText(sContext, R.string.module_activated, Toast.LENGTH_SHORT).show();
-
-                if (intent.hasExtra(EXTRA_ACTIVATE_MODULE_AND_RETURN)) return;
-            }
-
-            RootUtil rootUtil = new RootUtil();
-            if (!rootUtil.startShell()) {
-                Log.e(XposedApp.TAG, "Could not start root shell");
-                return;
-            }
-
-            boolean isSoftReboot = intent.getBooleanExtra(EXTRA_SOFT_REBOOT, false);
-            rootUtil.reboot(isSoftReboot ? RootUtil.RebootMode.SOFT : RootUtil.RebootMode.NORMAL,
-                    new RootUtil.LogLineCallback());
-
-            AssetUtil.removeBusybox();
-        }
     }
 }
